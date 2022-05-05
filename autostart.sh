5ad521f16f289accc539bb07ccf17f69e0914682
denizkendirci <91504649+denizkendirci@users.noreply.github.com>
2022-05-06 01:33:55 +0300

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 xrandr -s 1920x1080 &
-xsetroot -name "MG" &
+xsetroot -name "Mortimer Graves" &
 nitrogen --restore &
 sxhkd -c /home/mortician/.config/sxhkd/sxhkdrc &
 picom -c /home/mortician/.config/picom/picom.conf &
